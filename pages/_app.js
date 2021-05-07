5e3c15a8c4b76c0b443b5296f22f6a58fd5d6758
hrqsn <haruki_sano@icloud.com>
2021-05-07 15:52:46 +0900

    fix: scroll bug

--- pages/_app.js
+++ pages/_app.js
@@ -8,16 +8,14 @@ import '@/styles/custom.css'
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    window.onload = function () {
-      window.onscroll = function () {
-        const elm = document.getElementsByClassName('scroll-fx')
-        const height = window.innerHeight
-        for (let i = 0; i < elm.length; i++) {
-          const rect = elm[i].getBoundingClientRect()
-          const pos = rect.top - height + height/5
-          if (pos < 0) {
-            elm[i].classList.add('inshow')
-          }
+    window.onscroll = function () {
+      const elm = document.getElementsByClassName('scroll-fx')
+      const height = window.innerHeight
+      for (let i = 0; i < elm.length; i++) {
+        const rect = elm[i].getBoundingClientRect()
+        const pos = rect.top - height + height/5
+        if (pos < 0) {
+          elm[i].classList.add('inshow')
         }
       }
     }
