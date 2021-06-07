c95efbe1ad1349e4636a4718afe029fb1d2464a4
hrqsn <haruki_sano@icloud.com>
2021-06-07 21:34:13 +0900

    change: 文面を変更

--- pages/reference.js
+++ pages/reference.js
@@ -24,7 +24,7 @@ export default function Visit () {
       <main className='max-w-screen-md mx-auto px-4'>
         <section className='relative pt-32'>
           <p>Imagination Serverでは、より正確な再現を行うため、実際のパークで撮影した資料（写真や映像）を募集しています。</p>
-          <p>ご協力いただいた際は、私たちのワールドにお名前を記載させていただきます。</p>
+          <p>ご協力いただいた際は、ご希望に応じて私たちのワールドにお名前を記載させていただきます。</p>
           <p className='mt-4'>皆様のご協力をキャスト一同、心よりお待ちしております✨</p>
           <p className='mt-4 text-sm text-gray-500'>＊頂いた資料は再現時の使用に留め、外部に公開することはありません。</p>
           <a href="https://forms.gle/Y2mPhrRB6f24Z2U76" target="_blank" rel="noopener" className='flex mt-8 font-semibold text-blue-600'>フォームはこちら →</a>
