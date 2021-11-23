840d5c054fc6b0b02654ae686b83e89be9c51869
hrqsn <halsakuragi@gmail.com>
2021-11-24 08:38:10 +0900

    add: google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,25 @@
-import Providers from '@/components/providers'
 import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Providers from '@/components/providers'
+import * as gtag from '@/lib/gtag'
 
 import '@/styles/main.css'
 import '@/styles/chrome-bug.css'
 import '@/styles/prose.css'
 
 function MyApp ({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+  
   useEffect(() => {
     window.onscroll = function () {
       const elm = document.getElementsByClassName('scroll-fx')
